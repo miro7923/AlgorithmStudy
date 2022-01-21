b41e452025f65175c07d4bf0571b6ebc7e6ae9c5
KIMBYUNGWOO <59406944+kbwoo00@users.noreply.github.com>
2022-01-21 09:22:12 +0900

    [BOJ] 피보나치 함수 / 실버3
    https://www.acmicpc.net/problem/1003

--- ByungWooKim/DP/BOJ_1003.java
+++ ByungWooKim/DP/BOJ_1003.java
@@ -1,5 +1,3 @@
-package AlgorithmStudy.ByungWooKim.DP;
-
 /*
  * # 문제 주소
  * https://www.acmicpc.net/problem/1003
@@ -45,40 +43,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ_1003 {
+public class Main {
 
-    static int N;
+    static int N; // 테스트 케이스 개수
     static BufferedReader br;
-    static int[] inputValues;
-    static int[][] answers;
+    static int[] inputValues; // 입력받는 값들
+    static int[][] answers; // 0이 출력되는 횟수는 [n][0]에 1이 출력되는 횟수는[n][1]에 넣는다.
+
 
     static void input() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        inputValues = new int[N+1];
+        inputValues = new int[N];
+    }
 
-
+    static void calculate() throws IOException {
         for (int i = 0; i < N; i++) {
             inputValues[i] = Integer.parseInt(br.readLine());
             answers = new int[inputValues[i]+1][2];
-            answers[0][0] = 1;
-            answers[0][1] = 0;
+
+            answers[0][0] = 1;      answers[0][1] = 0;
             if(inputValues[i]==1){
-                answers[1][0] = 0;
-                answers[1][1] = 1;
+                answers[1][0] = 0;     answers[1][1] = 1;
             } else if (inputValues[i] >= 2){
+
                 fibonacci(inputValues[i]);
             }
             System.out.println(answers[inputValues[i]][0] + " " + answers[inputValues[i]][1]);
         }
-
     }
 
     static void fibonacci(int n) {
-        answers[0][0] = 1;
-        answers[0][1] = 0;
         answers[1][0] = 0;
         answers[1][1] = 1;
+
+        //입력받는 값이 2부터 0이 출력되는 횟수는 1 => 1 => 2 => 3 => 5 식으로 피보나치수열이
+        //입력받는 값이 2부터 1이 출력되는 횟수는 1 => 2 => 3 => 5 => 8 이다.
+        //1이 출력되는 횟수가 0이 출력되는 횟수보다 1번 빠른(?)피보나치 수열인 셈.
 
         if (n >= 2) {
             for (int i = 2; i < answers.length; i++) {
@@ -91,5 +92,11 @@ public class BOJ_1003 {
 
     public static void main(String[] args) throws IOException {
         input();
+        calculate();
     }
 }
+
+/*
+입력 케이스 하나마다 출력해줘야 했는데 처음에 입력 케이스 모두를 입력받고 한번에 출력하려고 하니 계속 런타임 오류 에러가 발생했다...
+문제를 잘 읽어보자!!
+ */
