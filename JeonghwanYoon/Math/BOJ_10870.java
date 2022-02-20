fae1b04f7193458e98e5f41351ed69fdcae7e040
yoonjh8080@gmail.com <yoonjh8080@gmail.com>
2022-02-20 20:33:17 +0900

    [BOJ]피보나치 수 5 / 브론즈2

---
+++ JeonghwanYoon/Math/BOJ_10870.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10870 {
+
+	public static void main(String[] args) throws IOException{
+		
+		/*
+		 * 문제 피보나치 수는 0과 1로 시작한다. 0번째 피보나치 수는 0이고, 1번째 피보나치 수는 1이다. 그 다음 2번째 부터는 바로 앞 두
+		 * 피보나치 수의 합이 된다.
+		 * 
+		 * 이를 식으로 써보면 Fn = Fn-1 + Fn-2 (n ≥ 2)가 된다.
+		 * 
+		 * n=17일때 까지 피보나치 수를 써보면 다음과 같다.
+		 * 
+		 * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+		 * 
+		 * n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
+		 * 
+		 * 입력 첫째 줄에 n이 주어진다. n은 20보다 작거나 같은 자연수 또는 0이다.
+		 * 
+		 * 출력 첫째 줄에 n번째 피보나치 수를 출력한다.
+		 * 
+		 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int num1 = 0;
+		int num2 = 1;
+		int pibo = 0;
+		
+		for (int i=2; i<=N; i++) {
+			pibo = num1 + num2;
+			num1 = num2;
+			num2 = pibo;
+		}
+		
+		if (N==0) {
+			System.out.println(num1);
+		}
+		
+		else if (N==1) {
+			System.out.println(num2);
+		}
+		
+		else {
+			System.out.println(pibo);
+		}
+	}
+}
